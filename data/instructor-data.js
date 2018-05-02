25496bc28c9b41d9589795f8833f74cc8d1e2298
Jordan Pratt <jpratt@mtroyal.ca>
2018-05-02 22:25:07 +0000

    Add more instructors.

--- data/instructor-data.js
+++ data/instructor-data.js
@@ -5,5 +5,57 @@ export let instructors = [{
     {
         id: "ppospisil",
         name: "Paul Pospisil"
+    },
+    {
+        id: "rconnolly",
+        name: "Randy Connolly"
+    },
+    {
+        id: "sittyipe",
+        name: "Shoba Ittyipe"
+    },
+    {
+        id: "pperri",
+        name: "Patrick Perri"
+    },
+    {
+        id: "nkhemka",
+        name: "Namrata Khemka"
+    },
+    {
+        id: "tkendon",
+        name: "Tyson Kendon"
+    },
+    {
+        id: "chepler",
+        name: "Charles Hepler"
+    },
+    {
+        id: "fuzoka",
+        name: "Michael Uzoka"
+    },
+    {
+        id: "ryumol",
+        name: "Ruben Yumol"
+    },
+    {
+        id: "jheard",
+        name: "Jason Heard"
+    },
+    {
+        id: "lmarik",
+        name: "Laura Marik"
+    },
+    {
+        id: "mgong",
+        name: "Mingwei Gong"
+    },
+    {
+        id: "kbecker",
+        name: "Katrin Becker"
+    },
+    {
+        id: "ttmosher",
+        name: "Teri Tibbel-Mosher"
     }
 ];
